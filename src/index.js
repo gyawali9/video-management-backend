1d7e98f5899d0475e79c645cd8dd0a0a2ef0551d
gyawali9 <roshangyawali9@gmail.com>
2025-04-07 20:00:00 +0545

    feat: utility funcitons for custom error and apiresponse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import "dotenv/config";
+// import "dotenv/config";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 // dotenv.config({
 //   path: "./env",
 // });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running ar port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MONGO DB connection failed !!!", err));
